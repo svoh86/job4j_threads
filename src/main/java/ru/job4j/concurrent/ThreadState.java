e7ed5b139f925f447fe2e3ea2e19e85514fee615
svoh86 <sms-86@mail.ru>
2022-07-25 20:33:59 +0300

    Состояние нити. [#229175 #325699]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -19,7 +19,7 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-               && second.getState() != Thread.State.TERMINATED) {
+               || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState() + " " + second.getState());
         }
         System.out.println("Работа завершена");
