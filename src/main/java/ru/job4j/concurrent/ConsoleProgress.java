6d65408a218b1650430d95de6cd78a3b704f2e9e
svoh86 <sms-86@mail.ru>
2022-07-26 11:33:24 +0300

    Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -18,7 +18,7 @@ public class ConsoleProgress implements Runnable {
                 i++;
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
     }
@@ -26,7 +26,7 @@ public class ConsoleProgress implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         progress.interrupt();
     }
 }
