99a093837ed99ee78792adbe7af4f48f5f81edf1
svoh86 <sms-86@mail.ru>
2022-07-29 10:38:24 +0300

    Immutable объекты [#267918 #326392]

--- src/main/java/ru/job4j/linked/Node.java
+++ src/main/java/ru/job4j/linked/Node.java
@@ -3,13 +3,14 @@ package ru.job4j.linked;
 /**
  * 3. Immutable объекты
  * Правила создания Immutable объекта:
- * 1. Все поля отмечены final.
+ * 1. Все поля отмечены private final.
  * 2. Состояние объекта не изменяется после создания объекта (убрать Сеттеры и т.д.)
+ * 3. Запретить наследование
  *
  * @author Svistunov Mikhail
  * @version 1.0
  */
-public class Node<T> {
+public final class Node<T> {
     private final Node<T> next;
     private final T value;
 
