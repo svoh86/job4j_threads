288b8e47ec4db7a9ba9ffc2527bef8b8fb459958
svoh86 <sms-86@mail.ru>
2022-08-17 16:41:24 +0300

    ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,53 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 2. ExecutorService рассылка почты.
+ *
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public class EmailNotification {
+    private final ExecutorService pool;
+
+    public EmailNotification() {
+        this.pool = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors()
+        );
+    }
+
+    /**
+     * Метод должен через ExecutorService отправлять почту.
+     * Через ExecutorService создайте задачу,
+     * которая будет создавать данные для пользователя и передавать их в метод send.
+     *
+     * @param user user
+     */
+    public void emailTo(User user) {
+        pool.submit(() -> {
+                    String subject = String.format("Notification %s to email %s", user.username(), user.email());
+                    String body = String.format("Add a new event to %s", user.username());
+                    send(subject, body, user.email());
+                }
+        );
+    }
+
+    /**
+     * Метод закрывает pool
+     */
+    public void close() {
+        pool.shutdown();
+    }
+
+    /**
+     * Метод должен быть пустой.
+     *
+     * @param subject subject
+     * @param body    body
+     * @param email   email
+     */
+    public void send(String subject, String body, String email) {
+    }
+}
---
+++ src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,11 @@
+package ru.job4j.pool;
+
+/**
+ * 2. ExecutorService рассылка почты.
+ * Модель данных
+ *
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public record User(String username, String email) {
+}
