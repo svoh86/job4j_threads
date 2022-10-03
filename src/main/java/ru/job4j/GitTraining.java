b430a647003835b1f1d3bd9ec360b476df1a94f1
svoh86 <sms-86@mail.ru>
2022-10-03 10:29:28 +0300

    3.2. Откат изменений [#504830]

---
+++ src/main/java/ru/job4j/GitTraining.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+/**
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+public class GitTraining {
+
+    public static void main(String[] args) {
+        System.out.println("bla");
+    }
+}
