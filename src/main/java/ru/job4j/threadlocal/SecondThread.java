a174d7d35e1114ea2cb54fc6d082263b0c659d80
svoh86 <sms-86@mail.ru>
2023-07-18 10:37:57 +0300

    1. Ветки ver.6

--- src/main/java/ru/job4j/threadlocal/SecondThread.java
+++ src/main/java/ru/job4j/threadlocal/SecondThread.java
@@ -4,7 +4,7 @@ package ru.job4j.threadlocal;
  * @author Svistunov Mikhail
  * @version 1.0
  */
-public class SecondThread extends Thread{
+public class SecondThread extends Thread {
     @Override
     public void run() {
         ThreadLocalDemo.tl.set("Это поток 2.");
